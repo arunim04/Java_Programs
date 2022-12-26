9016a90ead73d1a7467c5ed801f50b84bb163d41
arunim04 <arunimsureka@gmail.com>
2022-12-26 15:33:58 +0530

    adding factorial file

---
+++ JavaDS/factorial.java
@@ -0,0 +1,16 @@
+package JavaDS;
+import java.util.*;
+public class factorial {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no. to find factorial");
+        int a=sc.nextInt();
+        System.out.println(fact(a));
+    }
+
+    public static int fact(int n){
+        if(n<=1)
+            return 1;
+        return fact(n-1)*n;
+    }
+}
