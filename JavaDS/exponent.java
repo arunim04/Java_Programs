aade683bd0d95fa241eec44abcb7569692015ea9
arunim04 <arunimsureka@gmail.com>
2022-12-27 18:47:20 +0530

    exponent file added

---
+++ JavaDS/exponent.java
@@ -0,0 +1,30 @@
+package JavaDS;
+
+import java.util.Scanner;
+
+public class exponent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter base");
+        int a = sc.nextInt();
+        System.out.println("enter exponent");
+        int b = sc.nextInt();
+        System.out.println(expoo(a,b));
+    }
+    // Time: O(n), Space: O(1)
+    public static int expo(int a,int b){
+        int c=1;
+        for(int i=1;i<=b;i++){
+            c=c*a;
+        }
+        return c;
+    }
+    // Time: O(log n), Space: O(n)
+    public static int expoo(int a,int b){
+        if(b==0)
+            return 1;
+        if (b % 2 == 0)
+            return expoo(a * a, b / 2);
+        return a * expoo(a, b-1);
+    }
+}
