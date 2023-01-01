03bb93798a69cac01214a3f14ae9ef41625d438a
arunim04 <arunimsureka@gmail.com>
2023-01-01 20:30:46 +0530

    subarraysum1 code added

--- JavaDS/onedarray/subarraysum1.java
+++ JavaDS/onedarray/subarraysum1.java
@@ -2,29 +2,31 @@ package JavaDS.onedarray;
 
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 // Time: O(n), Space: O(n)
 
-public class subarraysum {
+public class subarraysum1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the length of the array");
         int a = sc.nextInt();
-        int[] num=new int[a];
-        for(int i=0;i<a;i++)
-            num[i]=sc.nextInt();
-        System.out.println(checksum(num,a));
+        System.out.println("enter the sum required");
+        int b=sc.nextInt();
+        int[] num = new int[a];
+        for (int i = 0; i < a; i++)
+            num[i] = sc.nextInt();
+        System.out.println(checksum(num, a,b));
     }
-    public static boolean checksum(int[] num,int a){
-        HashSet<Integer> b=new HashSet<>();
+    public static boolean checksum(int[] num,int a,int b){
+        HashSet<Integer> c=new HashSet<>();
         int sum=0;
         for(int i=0;i<a;i++) {
-            b.add(sum);
+            c.add(sum);
             sum=sum+num[i];
-            if(b.contains(sum))
+            if(c.contains(sum-b))
                 return true;
         }
         return false;
     }
 }
+
