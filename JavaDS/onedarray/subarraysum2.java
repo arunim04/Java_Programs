45e1e9c2dd463e306eef1eabd6bcb5b73a55cd75
arunim04 <arunimsureka@gmail.com>
2023-01-01 21:46:09 +0530

    subarraysum2 code added

---
+++ JavaDS/onedarray/subarraysum2.java
@@ -0,0 +1,36 @@
+package JavaDS.onedarray;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+
+// Time: O(n), Space: O(n)
+
+public class subarraysum2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of the array");
+        int a = sc.nextInt();
+        System.out.println("enter the sum required");
+        int b=sc.nextInt();
+        int[] num = new int[a];
+        for (int i = 0; i < a; i++)
+            num[i] = sc.nextInt();
+        checksum(num,b);
+    }
+    public static void checksum(int[] num,int b){
+        Map<Integer,Integer> c=new HashMap<>();
+        int sum=0;
+        for(int i=0;i<num.length;i++) {
+            c.put(num[i],i);
+        }
+        for(int i=0;i<num.length;i++){
+            if(c.containsKey(b-num[i]) && i!=c.get(b-num[i])) {
+                System.out.println(i + " " + c.get(b - num[i]));
+                c.remove(num[i]);
+                c.remove(b-num[i]);
+            }
+        }
+    }
+}
