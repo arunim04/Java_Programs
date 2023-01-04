3e234248a1caddb9fc7ea5ea656f3a4715a59a18
arunim04 <arunimsureka@gmail.com>
2023-01-04 15:36:57 +0530

    stock profit code changed

--- JavaDS/onedarray/stockprofit.java
+++ JavaDS/onedarray/stockprofit.java
@@ -14,7 +14,7 @@ public class stockprofit {
         for(int i=0;i<a;i++){
             arr[i]= sc.nextInt();
         }
-        System.out.println(maxpro(arr,a));
+        System.out.println(maxproday(arr,a));
     }
     public static int maxpro(int[] arr,int n){
         int maxprofit=0;
@@ -23,5 +23,29 @@ public class stockprofit {
                 maxprofit+=arr[i]-arr[i-1];
         }
         return maxprofit;
+    }
+    public static int maxproday(int[] arr,int n){
+        int minpos=0;
+        int totmaxpro=0;
+        int maxpro=0;
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[i-1])
+                maxpro+=arr[i]-arr[i-1];
+            else{
+                if(maxpro>0){
+                    totmaxpro+=maxpro;
+                    maxpro=0;
+                    System.out.print("buy on day"+minpos+"");
+                    System.out.print("sell on day"+(i-1));
+                }
+                minpos=i;
+            }
+        }
+        if(maxpro>0){
+            totmaxpro+=maxpro;
+            System.out.print("buy on day"+minpos+"");
+            System.out.print("sell on day"+(n-1));
+        }
+        return totmaxpro;
     }
 }
